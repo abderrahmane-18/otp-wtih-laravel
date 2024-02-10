70616f75840b9e579e8e9eb9865dd383b4d3dd2d
abderrahmane-18 <abderrahmane.boukhezar@gmail.com>
2024-02-10 11:36:23 +0100

    create function to generate code

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable
         'code',
         'expire',
     ];
-
+ 
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -44,4 +44,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function code_generaion()
+    {
+        $this->timestamps=false;
+        $this->code=rand(1000,9999);// set a code value randomly 1000 ....9999 
+        $this->expire=now()->addMinute(20);//add 20 min to time to expirate code , It remains active for 20 minutes and changes
+        $this->save();
+        
+ 
+    }
+
 }
