a2603440d4c2fd7fd005d88c65cd32b1cecd7b02
abderrahmane-18 <abderrahmane.boukhezar@gmail.com>
2024-02-10 12:50:42 +0100

    make  contoller and middleware

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('code')->nullable();
-            $table->dateTime('expire');
+            $table->dateTime('expire')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
